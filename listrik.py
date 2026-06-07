424d1e23f4e2b96678f12a5a9313d9687ce785bf
abyanyusufarrizkysaragih-glitch <abyanyusufarrizkysaragih@gmail.com>
2026-06-07 13:37:09 +0700

    terapkan coding standards dan refactor: pecah logika menjadi fungsi-fungsi kecil

--- listrik.py
+++ listrik.py
@@ -1,23 +1,32 @@
-def bayar(n, d, kwh):
-    t = 0
-    if d == 900:
-        t = kwh * 1352
-    elif d == 1300:
-        t = kwh * 1444
-    elif d == 2200:
-        t = kwh * 1699
+TARIF_LISTRIK = {
+    900: 1352,
+    1300: 1444,
+    2200: 1699,
+}
 
-    if kwh > 100:
-        t = t + (t * 0.05)
+PAJAK_RATE = 0.05
+BATAS_PEMAKAIAN_TINGGI = 100
+BATAS_PEMAKAIAN_RENDAH = 20
+SURPLUS_RATE = 0.05
+POTONGAN_HEMAT = 10000
 
-    if kwh < 20:
-        t = t - 10000
+def hitung_tagihan_listrik(nama_pelanggan, daya, pemakaian_kwh):
+    tarif_per_kwh = TARIF_LISTRIK.get(daya, 0)
+    total_tagihan = pemakaian_kwh * tarif_per_kwh
 
-    p = t * 0.05
-    t = t + p
+    if pemakaian_kwh > BATAS_PEMAKAIAN_TINGGI:
+        print("Pemakaian tinggi, dikenakan biaya surplus 5%")
+        total_tagihan = total_tagihan + (total_tagihan * SURPLUS_RATE)
 
-    print("Nama: " + n)
-    print("Tagihan: " + str(t))
-    return t
+    if pemakaian_kwh < BATAS_PEMAKAIAN_RENDAH:
+        print("Pemakaian hemat, mendapat potongan 10000")
+        total_tagihan = total_tagihan - POTONGAN_HEMAT
 
-bayar("Andi", 1300, 120)
+    pajak = total_tagihan * PAJAK_RATE
+    total_tagihan = total_tagihan + pajak
+
+    print("Nama pelanggan: " + nama_pelanggan)
+    print("Total tagihan: " + str(total_tagihan))
+    return total_tagihan
+
+hitung_tagihan_listrik("Andi", 1300, 120)
